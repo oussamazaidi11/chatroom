101a2e28cd3cd9284085b47dbf8ff32003517988
oussamazaidi11 <oussamazaidi499@gmail.com>
2025-01-29 21:09:40 +0100

    file change

---
+++ Frontend/src/layouts/roomlayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom"
+import profile from '../zone user/Profile';
+import Footer from "../component/footer";
+
+const roomlayout = () => {
+  return <div>
+    
+
+    
+  </div>;
+};
+
+export default roomlayout;
