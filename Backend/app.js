caaec2018aea326b768ab94336191d1617aa27e1
oussamazaidi11 <oussamazaidi499@gmail.com>
2025-01-30 21:11:31 +0100

    file change

---
+++ Backend/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.listen(PORT, () => console.log("server is running on port" + PORT));
