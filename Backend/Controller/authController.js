f351ede351337e0afe1d51103061d7c56144790b
oussamazaidi11 <oussamazaidi499@gmail.com>
2025-02-03 00:52:30 +0100

    last version

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -1,2 +1,4 @@
 // configure the back with generating token 
 // transporter for verifyinn mail  
+const jwt=require("jsonwebtoken")
+const bcrypt=require("")
--- Backend/Model/userSchema.js
+++ Backend/Model/userSchema.js
@@ -1,1 +1,20 @@
-// configure a user schéma && rooms schéma 
+// configure a user schéma && rooms schéma
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please insert ur name"],
+  },
+  email: {
+    type: String,
+    required: [true, "insert email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "insert password"],
+  },
+});
+module.exports = ("User", userSchema);
+//userSchema as User
+//ALIAS USERRR §§§§§§
