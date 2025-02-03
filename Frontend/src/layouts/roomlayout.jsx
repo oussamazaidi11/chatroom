61451fe07a696be4c67963e3fb5bd100a051aca6
oussamazaidi11 <oussamazaidi499@gmail.com>
2025-02-03 17:15:42 +0100

    env updated

--- Frontend/src/layouts/roomlayout.jsx
+++ Frontend/src/layouts/roomlayout.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Outlet } from "react-router-dom"
-import profile from '../zone user/Profile';
-import Footer from "../component/footer";
+import { Outlet } from "react-router-dom";
+
+import Footer from "../component/Footer";
 
 const roomlayout = () => {
-  return <div>
-    
-
-    
-  </div>;
+  return <div></div>;
 };
 
 export default roomlayout;
